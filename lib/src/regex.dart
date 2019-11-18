b7762a90d50f21d39cc69a608de639a86d63fed2
jili02 <zhimotians@gmail.com>
2019-11-18 17:12:17 +0800

    获取NearHuscarl/flutter_login的代码，方便直接修改

--- lib/src/regex.dart
+++ lib/src/regex.dart
@@ -1,4 +1,5 @@
 class Regex {
   // https://stackoverflow.com/a/32686261/9449426
   static final email = RegExp(r'^[^\s@]+@[^\s@]+\.[^\s@]+$');
+  static final chinesePhoneNumber = RegExp(r'/^1([38]\d|5[0-35-9]|7[3678])\d{8}$/');
 }
